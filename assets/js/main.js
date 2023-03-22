d17c16809d156942d2594c14bde0c66f4dd4ca69
jm181987 <jorgitom18@gmail.com>
2023-03-22 13:23:37 +0000

    ajustes responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,13 @@ $(document).ready(function() {
  
 
 });
+
+
+
+// Toggle menu on mobile devices
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('show-menu');
+});
